fffc610f585d14357ab1aceda936deb39894658b
David Waterman <dgwaterman@gmail.com>
2023-08-14 12:58:19 +0100

    Remove Evotec as sponsors

--- includes/sponsors.php
+++ includes/sponsors.php
@@ -5,6 +5,5 @@
       <a href="http://www.diamond.ac.uk"><img alt="Diamond Light Source" src="images/dls_weblogo.jpg" align="right">
       <a href="http://journals.iucr.org/d"><img alt="IUCr" src="images/IUCr.png" align="right">
       <a href="https://www.dectris.com"><img alt="DECTRIS" src="images/dectris.png" align="right">
-      <a href="https://www.evotec.com"><img alt="Evotec" src="images/Evotec.png" align="right">
    </div>
 </footer>

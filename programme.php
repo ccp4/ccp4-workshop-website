43bec0d6c050aa339c9e8e2bde8dca9869eb65eb
David Waterman <dgwaterman@gmail.com>
2023-09-14 15:57:07 +0100

    pdf styles

--- programme.php
+++ programme.php
@@ -4,6 +4,30 @@
         <title>DLS-CCP4 Workshop</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" href="styles/ccp4.css" />
+      <style type="text/css">
+<!--
+
+#pdf {
+        width: 1337px;
+        height: 975px;
+        resize: both;
+        overflow: hidden;
+        margin: 2em auto;
+        border: 2px solid #6699FF;
+}
+
+#pdf p {
+   padding: 1em;
+}
+
+#pdf object {
+   display: block;
+   border: solid 1px #666;
+}
+
+-->
+</style>
+
     </head>
     <body>
         <?PHP include("includes/banner.php"); ?>

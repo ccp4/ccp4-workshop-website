f45a424112a2e62b3c130f4c970555742559bf4d
David Waterman <dgwaterman@gmail.com>
2023-09-14 15:48:28 +0100

    Give up and go back to using <object> to embed the PDF, because the display was corrupted using pdf.js on Android Chrome browser

--- programme.php
+++ programme.php
@@ -1,28 +1,25 @@
 <!doctype html>
 <html>
-
-<head>
-  <title>DLS-CCP4 Workshop</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="styles/ccp4.css" />
-</head>
-
-<body>
-  <?PHP include("includes/banner.php"); ?>
-  <main>
-    <!--p><strong style="color: red;">Watch this space!</strong></p-->
-    <!--img width="100%" src="images/programme.png" /-->
-    <center>
-        <iframe
-            src="./web/viewer.html?file=DLS-CCP4-2023.pdf"
-            width=800px
-            height=600px
-            style="border: none;"
-        />
-    </center>
-
-  </main>
-  <?PHP include("includes/sponsors.php"); ?>
-</body>
-
+    <head>
+        <title>DLS-CCP4 Workshop</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="styles/ccp4.css" />
+    </head>
+    <body>
+        <?PHP include("includes/banner.php"); ?>
+        <main>
+            <p>The programme embedded below may also be downloaded <a href="DLS-CCP4-2023.pdf">here.</a> Although this programme
+                is close to being final, it may still be subject to small changes before the course begins.
+            </p>
+            <div id="pdf">
+                <object data="DLS-CCP4-2023.pdf" type="application/pdf" width="100%" height="100%">
+                    <p>It appears you don't have a PDF plugin for this browser.
+                        Instead you can <a href="DLS-CCP4-2023.pdf">click here to
+                        download the PDF file.</a>
+                    </p>
+                </object>
+            </div>
+        </main>
+        <?PHP include("includes/sponsors.php"); ?>
+    </body>
 </html>

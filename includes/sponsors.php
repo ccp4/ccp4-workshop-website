eb8ef65aada3e6a9a69ef320ba725eefd57298a0
David Waterman <dgwaterman@gmail.com>
2023-10-18 14:21:51 +0100

    IUCr sponsored poster prize :-)

--- includes/sponsors.php
+++ includes/sponsors.php
@@ -3,7 +3,7 @@
    <div class="spaced">
       <a href="http://www.stfc.ac.uk"><img alt="STFC" src="images/ukri-stfc.png"></a>
       <a href="http://www.diamond.ac.uk"><img alt="Diamond Light Source" src="images/dls_weblogo.jpg" align="right">
-      <!--a href="http://journals.iucr.org/d"><img alt="IUCr" src="images/IUCr.png" align="right"-->
+      <a href="http://journals.iucr.org/d"><img alt="IUCr" src="images/IUCr.png" align="right">
       <a href="https://www.dectris.com"><img alt="DECTRIS" src="images/dectris.png" align="right">
       <a href="https://www.mitegen.com/"><img src="images/mitegen_logo.jpg" align="right" height=50>
    </div>

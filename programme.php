f44bae693d17fbdc69f1d8f6e6b99c4043b38b0e
David Waterman <dgwaterman@gmail.com>
2024-07-18 16:56:40 +0100

    Update programme PDF

--- programme.php
+++ programme.php
@@ -32,11 +32,11 @@
     <body>
         <?PHP include("includes/banner.php"); ?>
         <main>
-            <p>The programme embedded below may also be downloaded <a href="DLS-CCP4-2023.pdf">here.</a> Although this programme
+            <p>The programme embedded below may also be downloaded <a href="DLS-CCP4-2024.pdf">here.</a> Although this programme
                 is close to being final, it may still be subject to small changes before the course begins.
             </p>
             <div id="pdf">
-                <object data="DLS-CCP4-2023.pdf" type="application/pdf" width="100%" height="100%">
+                <object data="DLS-CCP4-2024.pdf" type="application/pdf" width="100%" height="100%">
                     <p>It appears you don't have a PDF plugin for this browser.
                         Instead you can <a href="DLS-CCP4-2023.pdf">click here to
                         download the PDF file.</a>
